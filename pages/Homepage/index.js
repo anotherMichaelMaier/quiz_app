fc9f768791528e1d8281dd5b5e9abeaff4dafe9a
anotherMichaelMaier <117644039+anotherMichaelMaier@users.noreply.github.com>
2023-01-10 16:54:33 +0100

    first pages

---
+++ pages/Homepage/index.js
@@ -0,0 +1,15 @@
+export default function Homepage() {
+  return (
+    <>
+      <main>
+        <header>
+          <h1>Quiz App</h1>
+        </header>
+        <body></body>
+        <footer>
+          <nav></nav>
+        </footer>
+      </main>
+    </>
+  );
+}
